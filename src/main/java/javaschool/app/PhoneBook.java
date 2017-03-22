2eb547fc03ded8c69d2bce065a7d4db1ca708f54
Dima <dmitry.net@inbox.lv>
2017-03-22 20:55:06 +0200

    new feature to search by partial name

--- src/main/java/javaschool/app/PhoneBook.java
+++ src/main/java/javaschool/app/PhoneBook.java
@@ -24,6 +24,20 @@ public class PhoneBook {
     }
 
     @Command
+    public List<Record> find(String str) {
+        str = str.toLowerCase();
+        List<Record> result = new ArrayList<>();
+        for (Record r : recordList) {
+            String name = r.getName().toLowerCase();
+            if (name.contains(str)) {
+                result.add(r);
+            }
+        }
+
+        return result;
+    }
+
+    @Command
     public void addPhone(int id, String phone) {
         for (Record r : recordList) {
             if (r.getId() == id) {
